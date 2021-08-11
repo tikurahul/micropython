6e2711f56de2e3176b38430b0a633a50c224323b
Rahul Ravikumar <tikurahul@gmail.com>
2021-08-10 23:15:38 -0700

    Add an example method to the C module that adds integers

--- examples/usercmodule/cexample/examplemodule.c
+++ examples/usercmodule/cexample/examplemodule.c
@@ -10,7 +10,25 @@ STATIC mp_obj_t example_add_ints(mp_obj_t a_obj, mp_obj_t b_obj) {
     // Calculate the addition and convert to MicroPython object.
     return mp_obj_new_int(a + b);
 }
+
+// This is the function which will be called from Python as cexample.add_array([...]).
+STATIC mp_obj_t example_add_array(mp_obj_t a_obj) {
+    // Extract the ints from the micropython input objects.
+    size_t nitems;
+    const mp_obj_t *items;
+    mp_obj_get_array(a_obj, &nitems, (mp_obj_t **)&items);
+    int result = 0;
+    for(size_t i = 0; i < nitems; i++) {
+        mp_obj_t item = items[i];
+        int value = mp_obj_get_int(item);
+        result += value;
+    }
+    // Calculate the addition and convert to MicroPython object.
+    return mp_obj_new_int(result);
+}
+
 // Define a Python reference to the function above.
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(example_add_array_obj, example_add_array);
 STATIC MP_DEFINE_CONST_FUN_OBJ_2(example_add_ints_obj, example_add_ints);
 
 // Define all properties of the module.
@@ -21,6 +39,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_2(example_add_ints_obj, example_add_ints);
 STATIC const mp_rom_map_elem_t example_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_cexample) },
     { MP_ROM_QSTR(MP_QSTR_add_ints), MP_ROM_PTR(&example_add_ints_obj) },
+    { MP_ROM_QSTR(MP_QSTR_add_array), MP_ROM_PTR(&example_add_array_obj) },
 };
 STATIC MP_DEFINE_CONST_DICT(example_module_globals, example_module_globals_table);
 
